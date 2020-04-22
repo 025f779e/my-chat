2a09d0eedecb2060356655e627a64d6ebc50cfb8
CX55PP <e2d7bef6@protonmail.com>
2020-04-23 00:56:16 +0200

    Added build

---
+++ build/precache-manifest.291224c95b4556240da53796f8e988f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e15d81bbbc3cc2e43035550323c3ad4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8372a60f2f403c32704c",
+    "url": "./static/css/main.e1668fca.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "./static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8372a60f2f403c32704c",
+    "url": "./static/js/main.d9ab89cf.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
